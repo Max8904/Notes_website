d398a243e9554f5fab4fa496119a0cccef52aae6
Max <max284741991@gmail.com>
2023-10-18 00:03:48 +0800

    FEAT: React Router

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const ListItem = () => {
-  return (
-    <div>
-        <h3>List Item</h3>
-    </div>
-  )
+const ListItem = ({note}) => {
+    return (
+    <Link to={`note/${note.id}`}>
+        <h3>{note.body}</h3>
+    </Link>
+    )
 }
 
 export default ListItem
---
+++ src/pages/NotePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+    useParams
+} from 'react-router-dom'
+import notes from '../assets/data'
+
+const NotePage = (props) => {
+    let noteId = useParams().id
+
+    let note = notes.find(note => note.id === Number(noteId))
+
+    return (
+    <div>
+        <p>{note?.body}</p>
+    </div>
+    )
+}
+
+export default NotePage
