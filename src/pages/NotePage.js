44ab344e4599c5b57719c4644b5d951f0ae1b171
Max <max284741991@gmail.com>
2023-10-27 00:22:35 +0800

    FEAT: Update Note

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import {
     useParams,
-    Link
+    Link,
+    useNavigate
 } from 'react-router-dom'
 import notes from '../assets/data'
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
@@ -15,10 +16,27 @@ const NotePage = (props) => {
         getNote()
     }, [noteId])
 
+    const navigate = useNavigate()
+
     let getNote = async () => {
         let response = await fetch(`http://localhost:8000/notes/${noteId}`)
         let data = await response.json()
         setNote(data)
+    }
+
+    let updateNote = async () => {
+        await fetch(`http://localhost:8000/notes/${noteId}`,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ ...note, 'updated': new Date() })
+        })
+    }
+
+    let handleSubmit = () => {
+        updateNote()
+        navigate('/')
     }
 
     return (
@@ -26,12 +44,12 @@ const NotePage = (props) => {
         <div className='note-header'>
             <h3>
                 <Link to="/">
-                    <ArrowLeft />
+                    <ArrowLeft onClick={handleSubmit} />
                 </Link>
             </h3>
         </div>
 
-        <textarea value={note?.body}>
+        <textarea onChange={(e) => { setNote({...note, 'body': e.target.value}) }} value={note?.body}>
 
         </textarea>
     </div>
