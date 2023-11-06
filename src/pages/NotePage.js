36b0ff9cbc90d5664aa7d244e22dcdbc02f5f651
Max <max284741991@gmail.com>
2023-11-06 22:59:10 +0800

    FIX: remove unused line

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -4,7 +4,6 @@ import {
     Link,
     useNavigate
 } from 'react-router-dom'
-import notes from '../assets/data'
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
 const NotePage = (props) => {
